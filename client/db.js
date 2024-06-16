8f8323183af49571da13d23162d75e2d1da75f40
MandemGibson <themaingib@gmail>
2024-06-16 19:04:12 +0000

    Changed PORT to DB_PORT

--- client/db.js
+++ client/db.js
@@ -6,7 +6,7 @@ const client = new Client({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.PORT),
+  port: Number(process.env.DB_PORT),
 });
 
 client
