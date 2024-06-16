8c0b2d1c31520073bec383859ddbcf47fbe30f02
MandemGibson <123828180+MandemGibson@users.noreply.github.com>
2024-06-16 23:10:07 +0000

    Update server/routes/todo.routes.js
    Co-authored-by: Alexander Boakye <aalexanderkwaku@yahoo.co.uk>

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -11,13 +11,19 @@ const {
 } = require("../controller/todos.controller");
 const todoRouter = Router();
 
-todoRouter.get("/", fetchTodos);
-todoRouter.get("/:id", getTaskById);
-todoRouter.post("/", addTask);
+todoRouter
+.route('/')
+.get(fetchTodos)
+.post(addTask)
+.delete(clearTodos);
+
+todoRouter
+.route('/:id')
+.get(getTaskById);
+.put(markTaskAsDone);
+.patch(markTaskAsNotDone);
+.delete(removeTodo);
+
 todoRouter.put("/task/:id", editTaskById);
-todoRouter.put("/:id", markTaskAsDone);
-todoRouter.patch("/:id", markTaskAsNotDone);
-todoRouter.delete("/", clearTodos);
-todoRouter.delete("/:id", removeTodo);
 
 module.exports = todoRouter;
